c175d5bbe2d3a5250e4be006f09e4c8c41a520e8
test-open <76162523+test-open@users.noreply.github.com>
2021-01-30 20:26:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,8 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.111.1/g' package/base-files/files/bin/config_generate
-
-rm -f proto_l2tp.lua feeds/luci/protocols/luci-proto-ppp/luasrc/model/cbi/admin_network
-rm -f proto_pppoa.lua feeds/luci/protocols/luci-proto-ppp/luasrc/model/cbi/admin_network
-rm -f proto_pppossh.lua feeds/luci/protocols/luci-proto-ppp/luasrc/model/cbi/admin_network
-rm -f proto_pptp.lua feeds/luci/protocols/luci-proto-ppp/luasrc/model/cbi/admin_network
