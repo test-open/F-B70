c0c21db327b8a0a0c8d951fa76a4f2a2bdf34d98
test-open <76162523+test-open@users.noreply.github.com>
2021-02-12 17:32:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.111.1/g' package/base-files/files/bin/config_generate
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/msgid "Interface Info"/msgid "Base Setting"/g' package/lean/luci-app-zerotier/po/zh-cn/zerotier.po
+sed -i 's/msgstr "接口信息"/msgstr "基本设置"/g' package/lean/luci-app-zerotier/po/zh-cn/zerotier.po
+sed -i '$a \\' package/lean/luci-app-zerotier/po/zh-cn/zerotier.po
+sed -i '$a msgid "Interface Info"' package/lean/luci-app-zerotier/po/zh-cn/zerotier.po
+sed -i '$a msgstr "接口信息"' package/lean/luci-app-zerotier/po/zh-cn/zerotier.po
